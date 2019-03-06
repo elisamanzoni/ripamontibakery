5922765905d7de87eef8cc19e34d30781c684581
elisamanzoni <37081924+elisamanzoni@users.noreply.github.com>
2019-03-06 22:16:29 +0100

    zcsxz

--- java.js
+++ java.js
@@ -7,3 +7,12 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+function myFunction2() {
+  var x = document.getElementById("myLinks2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
